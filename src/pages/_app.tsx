8c08fb5ffee63cf04ae399bc4449ab9f89a3ad0f
createhb21 <alsghk9701@gmail.com>
2024-01-02 11:05:17 +0900

    feat: Project Scaffolding

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { type ReactElement } from 'react'
+import { globalStyles, theme } from '@/styles'
+import { ThemeProvider, Global } from '@emotion/react'
+import type { AppContext, AppInitialProps, AppLayoutProps } from 'next/app'
+import { type NextComponentType } from 'next/types'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/font.css'
+
+const App: NextComponentType<AppContext, AppInitialProps, AppLayoutProps> = ({
+  Component,
+  pageProps,
+}: AppLayoutProps) => {
+  const getLayout = Component.getLayout ?? ((page: ReactElement) => page)
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeProvider>
+  )
 }
+
+export default App
---
+++ src/styles/themes/breakPoint.ts
@@ -0,0 +1,14 @@
+export const breakPoint = {
+  value: {
+    mobile: 767,
+    tablet: 1229,
+  },
+  device: {
+    tablet: `(min-width:  767px)`,
+    desktop: `(min-width: 1229px)`,
+    maxTablet: `(max-width: 767px)`,
+    maxDesktop: `(max-width: 1229px)`,
+  },
+} as const
+
+export type BreakPointTheme = typeof breakPoint
